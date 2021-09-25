606858d6c8438f24a779b2cc9577c6ad8713e515
Tiên Nguyễn <Tien.Nguyen10@mii.com>
2021-09-25 15:56:01 +0700

    Create share parameter

---
+++ LearnRevitAPI/03_ShareParameter_WPF/CreateShareParameterCmd.cs
@@ -0,0 +1,53 @@
+﻿#region Namespaces
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using LearnRevitAPI.Lib;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Application = Autodesk.Revit.ApplicationServices.Application;
+
+#endregion Namespaces
+
+namespace LearnRevitAPI._03_ShareParameter_WPF
+{
+   [Transaction(TransactionMode.Manual)]
+   class CreateShareParameterCmd : IExternalCommand
+   {
+      public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+      {
+         UIApplication uiapp = commandData.Application;
+         UIDocument uidoc = uiapp.ActiveUIDocument;
+         Application app = uiapp.Application;
+         Document doc = uidoc.Document;
+
+         string sharedParaPath = OfficeUtils.GetPathDialog("Select share parameter",
+                                                            string.Concat("All files|*.*", 
+                                                            "|Text files|*.txt", 
+                                                            "|Excel spreadsheet files|*.xls, *.xlsx"));
+
+         string shareParaGroup = "REVIT API";
+         string shareParaName = "Test API";
+         string shareParaDesc = "Desc test API";
+
+         List<Category> listCategory = new List<Category>();
+         listCategory.Add(Category.GetCategory(doc, BuiltInCategory.OST_Walls));
+         listCategory.Add(Category.GetCategory(doc, BuiltInCategory.OST_Floors));
+
+         using (Transaction trans = new Transaction(doc))
+         {
+            trans.Start("Create share parameter");
+
+            ParameterUtils.CreateSharedParamater(doc, app, sharedParaPath, shareParaGroup,
+                                                 shareParaName, ParameterType.Area, BuiltInParameterGroup.PG_TEXT,
+                                                 shareParaDesc, listCategory, true, true, true);
+
+            trans.Commit();
+         }
+
+         return Result.Succeeded;
+      }
+   }
+}
---
+++ LearnRevitAPI/Lib/OfficeUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.DB;
+
+namespace LearnRevitAPI.Lib
+{
+   public static class OfficeUtils
+   {
+      public static string GetPathDialog(string title, string filter = "All files|*.*")
+      {
+         OpenFileDialog dlg = new OpenFileDialog();
+         dlg.Title = title;
+         dlg.Filter = filter;
+
+         if (System.Windows.Forms.DialogResult.OK != dlg.ShowDialog())
+         {
+            MessageBox.Show("Please select file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            return String.Empty;
+         }
+         else
+         {
+            return dlg.FileName;
+         }
+      }
+   }
+}
