dfb948687bd6e124bb0e4cd7419555f00a83dd4b
Tiên Nguyễn <Tien.Nguyen10@mii.com>
2021-12-28 23:30:58 +0700

    Rename views

--- LearnRevitAPI/09_RenameView/RenameViewWindow.xaml.cs
+++ LearnRevitAPI/09_RenameView/RenameViewWindow.xaml.cs
@@ -30,17 +30,56 @@ namespace LearnRevitAPI._09_RenameView
 
       private void btnOk_Click(object sender, RoutedEventArgs e)
       {
+         DialogResult = true;
+         //Close();
 
+         foreach (ViewExtension2 v in _viewModel.AllViewsToRename)
+         {
+            using (Transaction trans = new Transaction(_viewModel.Doc))
+            {
+               trans.Start("x");
+               try
+               {
+                  v.View.get_Parameter(BuiltInParameter.VIEW_NAME)
+                      .Set(v.NewName);
+               }
+               catch (Exception exception)
+               {
+               }
+
+               trans.Commit();
+            }
+         }
       }
 
       private void btnCancel_Click(object sender, RoutedEventArgs e)
       {
-
+         DialogResult = false;
+         Close();
       }
 
       private void btnRun_Click(object sender, RoutedEventArgs e)
       {
+         foreach (var v in _viewModel.AllViewsToRename)
+         {
+            // Add prefix, suffix
+            try
+            {
+               v.NewName
+                   = _viewModel.Prefix + v.Name + _viewModel.Suffix;
+            }
+            catch (Exception exception)
+            {
+            }
 
+            // find & replace
+            if (string.IsNullOrEmpty(_viewModel.FindValue) == false)
+            {
+               v.NewName
+                   = v.Name.Replace(_viewModel.FindValue,
+                       _viewModel.ReplaceValue); ;
+            }
+         }
       }
 
       private void btnAddViews_Click(object sender, RoutedEventArgs e)
